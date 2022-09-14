fca69202e0d3cf199fb04f91fd9e4e061911735c
sriharsha annamaneni <sriharsha0806@gmail.com>
2022-09-14 16:46:13 +0530

    Update typewise_alert.py

--- typewise_alert.py
+++ typewise_alert.py
@@ -1,33 +1,40 @@
-from collections import defaultdict 
-
 def infer_breach(value, lowerLimit, upperLimit):
-    if value < lowerLimit:
-        return 'TOO_LOW'
-    elif value > upperLimit:
-        return 'TOO_HIGH'
-    return 'NORMAL'
+  if value < lowerLimit:
+    return 'TOO_LOW'
+  if value > upperLimit:
+    return 'TOO_HIGH'
+  return 'NORMAL'
 
 def classify_temperature_breach(coolingType, temperatureInC):
-    coolingTypes = ['PASSIVE_COOLING', 'HI_ACTIVE_COOLING', 'MED_ACTIVE_COOLING']
-    upperLimit = [35, 45, 40]
-    return infer_breach(temperatureInC, 0, upperLimit[coolingTypes.index(coolingType)])\
-if coolingType in coolingTypes else infer_breach(temperatureInC, 0, 0)
-
-def def_value():
-    return "Not Present"
+    l = [35, 40, 45]
+    coolingTypes = ['PASSIVE_COOLING', 'MED_ACTIVE_COOLING', 'HI_ACTIVE_COOLING']
+    if temperatureInC in range(l[-1]+1):
+        lowerlimit = 0
+        index = coolingTypes.index(coolingType)
+        upperlimit = l[index]
+        return infer_breach(temperatureInc, lowerlimit, upperlimit)
+    else:
+        raise ValueError("Temperature is not within range")
 
 def check_and_alert(alertTarget, batteryChar, temperatureInC):
-    breachType = classify_temperature_breach(batteryChar['CoolingType'], temperatureInC)
-    d = defaultdict(def_value)
-    d["TO_CONTROLLER"] = send_to_controller(breachType)
-    d["TO_EMAIL"] = send_to_email(breachType)
-    return d[alertTarget]
+  breachType =\
+    classify_temperature_breach(batteryChar['coolingType'], temperatureInC)
+  if alertTarget == 'TO_CONTROLLER':
+    send_to_controller(breachType)
+  elif alertTarget == 'TO_EMAIL':
+    send_to_email(breachType)
+
 
 def send_to_controller(breachType):
-    header = 0xfeed
-    print(f'{header}, {breachType}')
+  header = 0xfeed
+  print(f'{header}, {breachType}')
+
 
 def send_to_email(breachType):
-    recepient = "a.b@c.com"
+  recepient = "a.b@c.com"
+  if breachType == 'TOO_LOW':
     print(f'To: {recepient}')
-    print('Hi, the temperature is', ' '.join(breachType.lower().split('_')))
+    print('Hi, the temperature is too low')
+  elif breachType == 'TOO_HIGH':
+    print(f'To: {recepient}')
+    print('Hi, the temperature is too high')
